9edcf54a1968654861abedbb0ecf22c39e69799a
Scott Gardner <scott.gardner@mac.com>
2014-04-21 03:29:59 -0500

    Initial commit

---
+++ CALayerSubclassTourTests/CALayerSubclassTourTests.m
@@ -0,0 +1,34 @@
+//
+//  CALayerSubclassTourTests.m
+//  CALayerSubclassTourTests
+//
+//  Created by Scott Gardner on 4/21/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CALayerSubclassTourTests : XCTestCase
+
+@end
+
+@implementation CALayerSubclassTourTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
